c4353d6110ef3b8a69b68f2a4a26f806834ee9d7
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-11-29 17:57:48 +0100

    Hacked limit script for LQTChannel analysis, but in a safe way

--- PostAnalyzer/functions/PlotLimits.py
+++ PostAnalyzer/functions/PlotLimits.py
@@ -6,8 +6,9 @@ from collections import OrderedDict
 from ROOT import gROOT, TFile, TCanvas, TH1D, TTree, TGraph, TGraphAsymmErrors, kBlack, kOrange, kGreen, kRed
 from array import array
 from tdrstyle_all import *
+import constants
 
-def PlotLimits(self, signal_scaled_by=1., draw_observed=False):
+def PlotLimits(self, signal_scaled_by=1., draw_observed=False, draw_theory=False):
     print green('  --> Plotting %s limits.' % ('expected' if not draw_observed else 'expected and observed'))
     # Expects all signals to belong to the same process!
 
@@ -16,8 +17,20 @@ def PlotLimits(self, signal_scaled_by=1., draw_observed=False):
     #LQLQToBTau_MLQ1000_MPS117_MC1100_L1p0
     limits_and_quantiles_per_mass = {}
     for signal in self.signals:
-        signaltag = signal.split('_')[0]
-        signalmass = signal.split('_')[1][3:]
+        parts = signal.split('_')
+        signaltag = ''
+        signalmass = -1
+        for part in parts:
+            if not 'MLQ' in part:
+                if signaltag == '': signaltag = part
+                else: signaltag = '_'.join([signaltag, part])
+            else:
+                signalmass = part[3:]
+                break
+
+        print signaltag, signalmass
+        # signaltag = signal.split('_')[0]
+        # signalmass = signal.split('_')[1][3:]
         limits_and_quantiles_per_mass[int(signalmass)] = {}
         combinefilename = os.path.join(self.combineoutput_path, 'higgsCombine%s.AsymptoticLimits.mH%s.root' % (signaltag, signalmass))
         rootfile = TFile(combinefilename, 'READ')
@@ -52,31 +65,60 @@ def PlotLimits(self, signal_scaled_by=1., draw_observed=False):
         npoints_95 += 1
 
     # get theory prediction
-    crosssectionfiletag  = self.signals[0].split('_')[0].split('To')[0]
-    if crosssectionfiletag == 'LQLQ':
-        pass
-    else:
-        raise ValueError('PlotLimits() can only plot LQLQ cross section. This seems to be a different process (maybe PsiPsi?) and needs implementation.')
-    crosssectiongraphname = '_'.join([crosssectionfiletag, self.signals[0].split('_')[3], self.signals[0].split('_')[4]])
-    xsecinfilename = os.path.join(self.crosssection_path, 'Crosssections_%s.root' % (crosssectionfiletag))
-    xsecinfile = TFile(xsecinfilename, 'READ')
-    g_theory = xsecinfile.Get(crosssectiongraphname)
+    if draw_theory:
+        if signaltag.startswith('LQLQ'):
+            xsecinfilename = os.path.join(self.crosssection_path, 'Crosssections_LQLQ.root')
+            crosssectiongraphname = '_'.join(['LQLQ', self.signals[0].split('_')[3], self.signals[0].split('_')[4]])
+        elif signaltag.startswith('LQTChannel'):
+            xsecinfilename = os.path.join(self.crosssection_path, 'Crosssections_%s.root' % (signaltag))
+            crosssectiongraphname = '_'.join([signaltag, 'MC1107', 'L1p0'])
+        else:
+            raise ValueError('PlotLimits() can only plot LQLQ and t-channel cross section. This seems to be a different process (maybe PsiPsi?) and needs implementation.')
+        xsecinfile = TFile(xsecinfilename, 'READ')
+        g_theory_realistic = xsecinfile.Get(crosssectiongraphname)
 
+    # hack some theory lines for illustration
+    if signaltag.startswith('LQTChannel'):
+        g_theory_equal = g_theory_realistic.Clone()
+        g_theory_realistic_L2p5 = g_theory_realistic.Clone()
+        for i in range(g_theory_equal.GetN()):
+            g_theory_equal.GetY()[i] *= 17.7 # equal couplings (both = 0.7) instead of (1 and 0.12) --> 0.7^4 / (1^2 * 0.12^2) = 17.7
+            g_theory_equal.GetEYhigh()[i] *= 17.7 # equal couplings (both = 0.7) instead of (1 and 0.12) --> 0.7^4 / (1^2 * 0.12^2) = 17.7
+            g_theory_equal.GetEYlow()[i] *= 17.7 # equal couplings (both = 0.7) instead of (1 and 0.12) --> 0.7^4 / (1^2 * 0.12^2) = 17.7
+        for i in range(g_theory_realistic_L2p5.GetN()):
+            g_theory_realistic_L2p5.GetY()[i] *= 39 # lambda = 2.5 instead of 1.0 --> * 2.5^4 = 39
+            g_theory_realistic_L2p5.GetEYhigh()[i] *= 39 # lambda = 2.5 instead of 1.0 --> * 2.5^4 = 39
+            g_theory_realistic_L2p5.GetEYlow()[i] *= 39 # lambda = 2.5 instead of 1.0 --> * 2.5^4 = 39
 
-    c = tdrCanvas(canvName='c', x_min=min(limits_and_quantiles_per_mass), x_max=max(limits_and_quantiles_per_mass), y_min=1E-5, y_max=1E2, nameXaxis='M_{LQ} [GeV]', nameYaxis='#sigma (pp #rightarrow LQLQ) [pb]', square=True, iPeriod=0, iPos=11)
+    yaxis_title = '#sigma (pp #rightarrow %s) [pb]' % (procname_to_latex[signaltag])
+    c = tdrCanvas(canvName='c', x_min=min(limits_and_quantiles_per_mass), x_max=max(limits_and_quantiles_per_mass), y_min=1E-5, y_max=1E4, nameXaxis='M_{LQ} [GeV]', nameYaxis=yaxis_title, square=True, iPeriod=0, iPos=11)
     c.SetLogy()
-    leg = tdrLeg(0.45,0.65,0.85,0.9)
+    leg = tdrLeg(0.5,0.65,0.85,0.9, textSize=0.042)
     tdrHeader(leg, '95% CL upper limits')
+    leg_theory = tdrLeg(0.5,0.4,0.85,0.65, textSize=0.042)
+    tdrHeader(leg_theory, 'Theory prediction')
     tdrDraw(g_95, "3 SAME", mcolor=kOrange, lcolor=kOrange, fcolor=kOrange)
     tdrDraw(g_68, "3 SAME", mcolor=kGreen+1, lcolor=kGreen+1, fcolor=kGreen+1)
     tdrDraw(g_expected, "L SAME", mcolor=kBlack, lcolor=kBlack, fcolor=kBlack, lstyle=2)
-    tdrDraw(g_theory, "3L SAME", mcolor=kRed+2, lcolor=kRed+2, fcolor=kRed+2, lstyle=1, alpha=0.4)
-    leg.AddEntry(g_theory, 'LQ pair production', 'LF')
+    if draw_theory:
+        if signaltag.startswith('LQLQ'):
+            tdrDraw(g_theory, "3L SAME", mcolor=kRed+2, lcolor=kRed+2, fcolor=kRed+2, lstyle=1, alpha=0.4)
+            leg_theory.AddEntry(g_theory, 'LQ pair production', 'LF')
+        elif signaltag.startswith('LQTChannel'):
+            tdrDraw(g_theory_realistic, "3L SAME", mcolor=kRed+2, lcolor=kRed+2, fcolor=kRed+2, lstyle=1, alpha=0.4)
+            leg_theory.AddEntry(g_theory_realistic, 'Realistic cpl., #lambda = 1.0', 'LF')
+            tdrDraw(g_theory_equal, "3L SAME", mcolor=kAzure-2, lcolor=kAzure-2, fcolor=kAzure-2, lstyle=1, alpha=0.4)
+            leg_theory.AddEntry(g_theory_equal, 'Equal cpl., #lambda = 1.0', 'LF')
+            tdrDraw(g_theory_realistic_L2p5, "3L SAME", mcolor=kGreen-2, lcolor=kGreen-2, fcolor=kGreen-2, lstyle=1, alpha=0.4)
+            leg_theory.AddEntry(g_theory_realistic_L2p5, 'Realistic cpl., #lambda = 2.5', 'LF')
     leg.AddEntry(g_expected, 'Median expected', 'L')
     leg.AddEntry(g_68, '68% expected', 'F')
     leg.AddEntry(g_95, '95% expected', 'F')
     leg.Draw('SAME')
+    if draw_theory:
+        leg_theory.Draw('SAME')
     c.RedrawAxis()
     outname = os.path.join(self.plotoutput_path, 'Limits.pdf')
     c.SaveAs(outname)
-    xsecinfile.Close()
+    if draw_theory:
+        xsecinfile.Close()

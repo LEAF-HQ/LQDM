35f365dd9497aa3044271e539c685d79c708a6e3
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-11-29 17:48:02 +0100

    Added more presel hists

--- include/LQDMPreselectionHists.h
+++ include/LQDMPreselectionHists.h
@@ -7,6 +7,7 @@
 #include <TROOT.h>
 #include "Analyzer/LQDM/include/LQDMEvent.h"
 #include "Analyzer/include/BaseHists.h"
+#include "Analyzer/LQDM/include/NeutrinoReconstruction.h"
 
 using namespace std;
 
@@ -25,6 +26,8 @@ public:
 
 protected:
 
-  shared_ptr<TH1D> hmetpt, hmetphi, hstmet, hstmet_rebin, hnpv, hnpvgood, hmemu, hmetau, hmmutau, hmtautau, hsumweights;
+  shared_ptr<TH1D> hmetpt, hmetphi, hstmet, hstmet_rebin, hht, hht_rebin, hlt, hlt_rebin, hmt_taumet, hmt_taumet_rebin, hmt_taumet_rebinlimit, hmt_taumet_rebinlimit2, hmt_mumet, hmt_mumet_rebin, hmt_mumet_rebinlimit, hmt_mumet_rebinlimit2, hmt_emet, hmt_emet_rebin, hmt_emet_rebinlimit, hmt_emet_rebinlimit2, hmemu, hmetau, hmmutau, hmtautau, hdphi_tau1_met, hptratio_tau1_met, hdphi_mu1_met, hptratio_mu1_met, hdphi_e1_met, hptratio_e1_met, hreco_wmass, hreco_wpt, hnpv, hnpvgood, hsumweights;
+
+  std::vector<double> bins2, bins3;
 
 };

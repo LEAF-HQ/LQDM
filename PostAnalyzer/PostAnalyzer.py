65cc3c6bbc261222c088b09891a1dc40b9218cd1
Arne Christoph Reimers <arne.reimers@cern.ch>
2021-11-29 18:01:28 +0100

    Minor

--- PostAnalyzer/PostAnalyzer.py
+++ PostAnalyzer/PostAnalyzer.py
@@ -9,10 +9,11 @@ import functions.Combine
 
 
 class PostAnalyzer:
-    def __init__(self, year, analysisname, t3_base_path, result_base_path, selectionname, systematics, backgrounds, signals, channels, histnames_in_out_per_category, limithisttag, processes_per_systematic, pdf_per_systematic, value_per_systematic):
+    def __init__(self, year, analysisname, t3_base_path, result_base_path, selectionstage, selectionname, systematics, backgrounds, signals, channels, histnames_in_out_per_category, limithisttag, processes_per_systematic, pdf_per_systematic, value_per_systematic):
         self.year = year
         self.analysisname  = analysisname
         self.t3_base_path  = t3_base_path
+        self.selectionstage = selectionstage
         self.selectionname = selectionname
         self.user = os.environ['USER']
         self.systematics = systematics
@@ -26,8 +27,8 @@ class PostAnalyzer:
         self.value_per_systematic = value_per_systematic
 
         self.crosssection_path   = os.path.join(os.environ['GENERATORPATH'], 'crosssections', 'ChiPsi')
-        self.finalselection_path = os.path.join(t3_base_path,     self.user, analysisname, year, 'Finalselection', selectionname)
-        self.resultpath          = os.path.join(result_base_path, self.user, analysisname, year, 'Finalselection', selectionname)
+        self.finalselection_path = os.path.join(t3_base_path,     self.user, analysisname, year, selectionstage, selectionname)
+        self.resultpath          = os.path.join(result_base_path, self.user, analysisname, year, selectionstage, selectionname)
         self.combineinput_path   = os.path.join(self.resultpath, 'data', 'combineinput')
         self.combineoutput_path  = os.path.join(self.resultpath, 'data', 'combineoutput')
         self.plotoutput_path     = os.path.join(self.resultpath, 'plots')

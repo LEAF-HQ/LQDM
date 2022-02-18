59f1c7a6861778be71cbafe22dfcd9c0b7904f85
Arne Christoph Reimers <arne.reimers@cern.ch>
2022-02-18 17:08:36 +0100

    moved LQDM-specific stuff from the LEAF/Generator

--- Generator/generate_ChiPsi_PsiPsiToLQChi.py
+++ Generator/generate_ChiPsi_PsiPsiToLQChi.py
@@ -26,10 +26,13 @@ import tdrstyle_all as TDR
 from CrossSectionRunner import *
 from GensimRunner import *
 
-processes = ['LQLQToBTau']
-mlqs     = [1000, 1400, 1800, 2200, 2600]
-mchs_exp = [2.0] # to reasonable precision, the LQ->btau decay kinematics do not depend on DM/Psi masses
-lambdas  = [1.0, 'best']
+processes = ['PsiPsiToLQChi']
+mlqs     = [1000, 4000, 7000, 10000]
+mchs_exp = [2.0, 2.33, 2.66, 3.0, 3.33]
+lambdas  = [1.0]
+
+
+
 
 
 general_settings = {
@@ -46,6 +49,8 @@ for lamb in lambdas:
         for exp in mchs_exp:
             mch = find_closest(sorted(preferred_configurations[prefmlq].keys()), 10**exp)
             mps = preferred_configurations[prefmlq][mch][0]
+            if not are_masses_allowed(mlq=prefmlq, mps=mps, mch=mch, particle_produced='Psi', decaychannels=['BTau', 'PsiChi']):
+                continue
 
             dict = OrderedDict()
             # dict keys: what to replace in cards
@@ -149,7 +154,7 @@ submit = True
 
 
 EventGenerator = GensimRunner(processnames=processes, tag=tag, individual_settings=individual_settings, general_settings=general_settings[campaign], workdir_slurm=workdir_slurm, workarea=workarea, basefolder=basefolder, cardfolder=cardfolder, mgfolder=mgfolder, generatorfolder=generatorfolder, gridpackfolder=gridpackfolder, arch_tag=arch_tag, cmssw_tag_gp=cmssw_tag_gp, T2_director=T2_director, T2_path=T2_path, T2_director_root=T2_director_root, T3_director=T3_director, T3_path=T3_path, campaign=campaign, folderstructure=folderstructure, maxindex=maxindex, nevents=nevents, submit=submit)
-# EventGenerator.ProduceCards()
+EventGenerator.ProduceCards()
 EventGenerator.SubmitGridpacks(runtime=(3,00,00))
 # EventGenerator.SubmitGenerationStep(generation_step='GENSIM', ncores=2, runtime=(3,00,00), mode='new')
 # EventGenerator.SubmitGenerationStep(generation_step='GENSIM', ncores=8, runtime=(3,00,00), mode='resubmit')
